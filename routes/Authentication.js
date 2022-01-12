ec9d7560543b132127a88b8160f1888d81872399
Ay44n-ej4z <ayaan.ejaz185@gmail.com>
2022-01-12 10:28:52 +0530

    done

--- routes/Authentication.js
+++ routes/Authentication.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
 const bodyParser = require('body-parser');
 const router = express.Router();
 var typeorm = require('typeorm');
@@ -6,7 +9,6 @@ const connection = require('../server')
 const Post = require('../post')
 const bcrypt = require('bcryptjs');
 const posts_table = "posts";
-
 router.use(bodyParser.json())
 
 
@@ -22,33 +24,55 @@ router.get('/login', async (req, res) => {
 })
 
 router.post('/posts', async (req, res) => {
-    // const hashPass 
-    const connection = typeorm.getConnection();
-    const { name, email, password = await bcrypt.hash(req.body.password, 12)} = req.body;
 
+    const connection = typeorm.getConnection();
+    const rounds = 10   
+    const { name, email, password } = req.body;
+    bcrypt.hash(password, rounds, async(err, hash) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        // console.log(hash)
+        // password = hash
     await connection.getRepository(posts_table)
-        .createQueryBuilder()
-        .insert()
-        .values({ name, email, password }).execute();
+    .createQueryBuilder()
+    .insert()
+    .values({ name, email, password: hash }).execute();
+      })
+    
 })
 
 router.post('/userLogin', async (req, res) => {
-    // const hashPass 
-    const connection = typeorm.getConnection();
-    const { email, password } = req.body;
+    const connection = typeorm.getConnection()
 
-    // await connection.getRepository(posts_table)
-    //     .createQueryBuilder()
-    //     .insert()
-    //     .values({ name, email, password }).execute();
-      
-        var result = await connection.getRepository(posts_table)
-        .createQueryBuilder()
-    .where("user.email = :email", { email: req.body.email})
-    res.status(200).json({
-        "Message": "Working",
-        result
-    })
+    var result = await connection.getRepository(posts_table)
+
+    const accessToken = jwt.sign({ email: req.body.email, password: req.body.password }, process.env.JWT_SECRET, async (err, token) => {
+        // console.log(err);
+        console.log(token);
+        // accToken = token
+        // console.log(accToken)
+
+        const passMatch = await bcrypt.compare(req.body.password, hash);
+        let userLogin = await result.findOne({ email: req.body.email, passMatch });
+        
+        console.log("Data ", userLogin);
+
+        // res.status(401).send()    
+        if (userLogin === undefined) {
+            res.send({
+                "Message": "user Not found"
+            })
+        } else {
+
+            res.status(200).json({
+                userLogin,
+                accessToken: token
+            })
+        }
+    });
 })
+
 
 module.exports = router;
